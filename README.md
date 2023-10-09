7b77c30d0a1272005c7833cdf7cea9680f8e334b
Oscar Juarez <Osskr@users.noreply.github.com>
2023-10-09 14:21:34 -0300

    terminada seccion 3

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
 1. Clonar repositorio
 2. Ejecutar ``` yarn install``` o ```npm install```
 3. Abrir el URL del proyecto
+4. ejecutar yarn dev
+5. instalar react query  ``` yarn add @tanstack/react-query ```
+6. instalar devTools react query  ``` yarn add @tanstack/react-query-devtools ```
+
+6. instalar axios  ``` yarn add axios ```
+
+
+
+
+
+
+### Notas
+* ``` {refetchOnWindowFocus:false} ``` Desactiva/Activa que la peticion se haga cada vez que se pone en foco la ventana 
+
+Estados de las peticiones  
+
+* Fresh  - Data recien obtenida "fresca", todavia no se vencio la data, podemos tener la confianza de que es la ultima data
+* Fetching - Se cambia cada vez que dispara la promesa que trae la informacion
+* Paused - React query puede pausar la promesa que trae la peticion 
+* Stale -  Caracteristica por defecto de cada una de las peticiones, es una forma de decir que la data esta vieja, ya paso su fecha de expiracion. le decimos a react-query que no confie en la data que esta en el cache.
+
+* isLoading vs isFetching - isLoading se refiere a cuando estamos cargando los datos por primera vez  y no tenemos nada de datos en cache, isFetching se dispara cada vez que estemos haciendo una peticion. usualmente vamos a usar el isLoading.
+
+* initialData y placeHolderData nos ayudan a para poder hacer algo con el tiempo de busqueda mientras se esta cargando la data
--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,68 @@
+import { LoadingIcon } from "../../shared/components/LoadingIcon"
+import { useLabels } from "../hooks/useLabels"
+import {FC} from 'react'
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName:string) => void;
+}
+
+
+export const LabelPicker: FC<Props> = ({selectedLabels, onChange}) => {
+
+  //usando fetch api
+  // const getLabels = async()=>{
+  //     const res = await fetch('https://api.github.com/repos/facebook/react/labels')
+  //     const data =  await res.json();
+  //     console.log(data)
+  //     return data;
+
+  // }
+
+
+  // // este codigo va dentro de un custom hook mas adelante
+  //  const getLabels = async(): Promise<Label[]>=>{
+
+  //   const { data }  = await githubApi.get<Label[]>('/labels')
+  //   console.log(data)
+  //   return data;
+  //  }
+
+//refetchOnWindowFocus:false Desactiva/Activa que la peticion se haga cada vez que se pone en foco la ventana 
+  // const labelsQuery = useQuery(
+  //   ['labels'],
+  //   getLabels,
+  //  {refetchOnWindowFocus:false}
+   
+  //  )
+
+  
+
+
+  // usando el customHook
+  const labelsQuery = useLabels()
+
+    if(labelsQuery.isLoading) // ! porque isLoading y no isFetching
+      return (<LoadingIcon/>)
+
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      {
+        labelsQuery.data?.map(label => (
+          <span
+            key={label.id}
+            className={`badge rounded-pill m-1 label-picker  ${selectedLabels.includes(label.name)? 'label-active' : ''}` }
+            style={{ border: `1px solid #${label.color}`, color: `${label.color}` }}
+            onClick={()=> onChange(label.name)}
+          >
+            {label.name}
+          </span>
+
+        ))
+      }
+
+
     </div>
   )
 }
